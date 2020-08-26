027abc1289a931a1aa4de3b15de9e18c40f19fb8
Chris Anderson <zsmyna@gmail.com>
2020-08-27 00:37:13 +1200

    Simplified Shaders

--- buildShader.sh
+++ buildShader.sh
@@ -2,9 +2,11 @@ cd "${0%/*}"
 #cd ./core/TerrainEngine/Private
 #find . -name "*.comp" -exec glslangValidator -V {} -o ./build/{}.spv \;
 
-glslangValidator -V ./core/TerrainEngine/Private/Shader/Rain.comp -o build/Rain.spv
-glslangValidator -V ./core/TerrainEngine/Private/Shader/Erode.comp -o build/Erode.spv
-glslangValidator -V ./core/TerrainEngine/Private/Shader/DumpSediment.comp -o build/Dump.spv
-glslangValidator -V ./core/TerrainEngine/Private/Shader/Combine.comp -o build/Combine.spv
+glslangValidator -V110 ./core/TerrainEngine/Private/Shader/Rain.comp -o build/Rain.spv
+glslangValidator -V110 ./core/TerrainEngine/Private/Shader/Erode.comp -o build/Erode.spv
+glslangValidator -V110 ./core/TerrainEngine/Private/Shader/DumpSediment.comp -o build/Dump.spv
+glslangValidator -V110 ./core/TerrainEngine/Private/Shader/Combine.comp -o build/Combine.spv
+
+find ./build -name "*.spv" -exec spirv-opt -O {} -o {} \;
 
 wait
